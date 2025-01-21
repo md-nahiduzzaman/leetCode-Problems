9abea9b93424836cfb268e407c8a1462b5e99e09
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2025-01-21 13:10:33 +0600

    problem 2704 solved

--- problem-2704.js
+++ problem-2704.js
@@ -1,1 +1,19 @@
-// problems
+function expect(val) {
+  return {
+    toBe: function (expected) {
+      if (val === expected) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+
+    notToBe: function (expected) {
+      if (val !== expect) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    },
+  };
+}
